2bc6da0bc173681ee7f596d39af2515835c83c64
woojtylaGG <wojciech.piskorski@cyfroweszkoly.pl>
2024-10-28 16:32:21 +0100

    dodanie funkcji z operacjami

--- AsyncOperationsCalculator/AsyncOperationsCalculator.js
+++ AsyncOperationsCalculator/AsyncOperationsCalculator.js
@@ -3,3 +3,32 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
     });
+
+    function askQuestion(query){
+        return new Promise((resolve) => rl.question(query, resolve));
+    }
+    function add(a, b, callback) {
+        setTimeout(() => {
+            callback(null, a + b);
+        }, 1000);
+    }
+    function multiply(a, b, callback) {
+        setTimeout(() => {
+            callback(null, a * b);
+        }, 1000);
+    }
+    function addPromise(a, b) {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(a + b);
+            }, 1000);
+        });
+    }
+    function multiplyPromise(a, b) {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(a * b);
+            }, 1000);
+        });
+    }
+    
