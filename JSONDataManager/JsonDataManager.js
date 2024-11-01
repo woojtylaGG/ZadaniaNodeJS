fd9b8ad321b8a4bbecf78a34efa5859bc16a9bf9
woojtylaGG <wojciech.piskorski@cyfroweszkoly.pl>
2024-11-01 20:46:18 +0100

    Dodanie Funkcji AskQuestion

--- JSONDataManager/JsonDataManager.js
+++ JSONDataManager/JsonDataManager.js
@@ -4,3 +4,28 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+function askQuestion(query){
+    return new Promise((resolve) => rl.question(query, resolve));
+}
+
+async function addNewObject(filePath){
+    const name = await askQuestion('Enter name: ');
+    const age = await askQuestion('Enter age: ');
+    const email = await askQuestion('Enter email: ');
+    const newObject = {
+        name,
+        age,
+        email
+    };
+    let data = [];
+    if(fs.existsSync(filePath)){
+        if (fs.existsSync(filePath)){
+            const fileData = fs.readFileSync(filePath);
+        data = JSON.parse(fileData);
+        }
+        data.push(newObject);
+        fs.writeFileSync(filePath, JSON.stringify(data, null,2));
+        console.log('Data added');
+    }
+
+}
