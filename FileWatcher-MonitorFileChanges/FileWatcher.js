4b20d49e2839dc75bcfd3c8d37c5f03144f1ddbf
woojtylaGG <wojciech.piskorski@cyfroweszkoly.pl>
2024-11-02 16:52:53 +0100

    Dodanie klasy FileWatcher

--- FileWatcher-MonitorFileChanges/FileWatcher.js
+++ FileWatcher-MonitorFileChanges/FileWatcher.js
@@ -1,2 +1,11 @@
 const fs = require('fs');
 const EventEmitter = require('events');
+
+class FileWatcher extends EventEmitter {
+    constructor(directoryToWatch, logFile = 'fileWatcher.log') {
+        super();
+        this.directoryToWatch = directoryToWatch;
+        this.logFile = logFile;
+        this.watch();
+    }
+}
